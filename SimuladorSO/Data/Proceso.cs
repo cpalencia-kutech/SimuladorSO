20811484686bf40d7437ca4c9824971a5dc19b53
Carlos Hernandez <cahpalencia@kutech.com.gt>
2022-05-15 19:40:31 -0600

    Proyecto inicial

---
+++ SimuladorSO/Data/Proceso.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimuladorSO.Data
+{
+    public class Proceso
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public int Size { get; set; }
+    }
+}
