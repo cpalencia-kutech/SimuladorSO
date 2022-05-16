468b84492de1f0d9331fa93cb5afe3433298f83d
Carlos Hernandez <cahpalencia@kutech.com.gt>
2022-05-15 21:17:34 -0600

    sandwitch estupido

--- SimuladorSO/Data/Proceso.cs
+++ SimuladorSO/Data/Proceso.cs
@@ -7,6 +7,6 @@ namespace SimuladorSO.Data
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime StartDate { get; set; }
-        public int Size { get; set; }
+        public int State { get; set; }
     }
 }
